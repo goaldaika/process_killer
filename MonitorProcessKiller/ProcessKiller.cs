b253658e745cab04a05a59f86fe765753fa83336
goaldaika <kris.phan1999@gmail.com>
2022-11-10 00:50:25 +0100

    first commit

---
+++ MonitorProcessKiller/ProcessKiller.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MonitorProcessKiller
+{
+    internal class ProcessKiller
+    {
+        public string ProcessName { get; set; }
+        public int MaximumLifeTime { get; set; }
+        public int Frequency { get; set; }
+        public ProcessKiller(string name, int maximumLifeTime, int frequency)
+        {
+            this.ProcessName = name;
+            this.MaximumLifeTime = maximumLifeTime;
+            this.Frequency = frequency;
+        }
+        public bool KillProcess()
+        {
+            TimeSpan lifeTime = default;
+
+            foreach (var process in Process.GetProcessesByName(ProcessName))
+            {
+                lifeTime = DateTime.Now - process.StartTime;
+                if (lifeTime.TotalMinutes >= MaximumLifeTime)
+                {
+                    process.Kill();
+                    WriteLog();
+                }
+            }
+
+            return true;
+        }
+        public bool WriteLog()
+        {
+            if (File.Exists("ProcessKilled.xml"))
+            {
+                XmlDocument processedData = new XmlDocument();
+                processedData.Load("ProcessKilled.xml");
+
+                XmlNode root = processedData.SelectSingleNode("/Root");
+
+                XmlNode processNode = processedData.CreateElement("Process");
+                root.AppendChild(processNode);
+
+                XmlAttribute nameAttr = processedData.CreateAttribute("Name");
+                nameAttr.Value = ProcessName;
+                processNode.Attributes.Append(nameAttr);
+
+                
+                foreach(var process in Process.GetProcessesByName(ProcessName))
+                {
+                    XmlNode startTimeNode = processedData.CreateElement("StartTime");
+                    startTimeNode.AppendChild(processedData.CreateTextNode(process.StartTime.ToString()));
+                    processNode.AppendChild(startTimeNode);
+                }
+                
+
+                XmlNode killedTimeNode = processedData.CreateElement("KilledTime");
+                killedTimeNode.AppendChild(processedData.CreateTextNode(DateTime.Now.ToString()));
+                processNode.AppendChild(killedTimeNode);
+
+
+                processedData.Save("ProcessKilled.xml");
+            }
+            else
+            {
+                XmlDocument processedData = new XmlDocument();
+                processedData.AppendChild(processedData.CreateXmlDeclaration("1.0", "UTF-8", "no"));
+
+                XmlNode root = processedData.CreateElement("Root");
+                processedData.AppendChild(root);
+                XmlNode processNode = processedData.CreateElement("Process");
+                root.AppendChild(processNode);
+
+                XmlAttribute nameAttr = processedData.CreateAttribute("Name");
+                nameAttr.Value = ProcessName;
+                processNode.Attributes.Append(nameAttr);
+
+                
+                foreach (var process in Process.GetProcessesByName(ProcessName))
+                {
+                    XmlNode startTimeNode = processedData.CreateElement("StartTime");
+                    startTimeNode.AppendChild(processedData.CreateTextNode(process.StartTime.ToString()));
+                    processNode.AppendChild(startTimeNode);
+                }
+
+
+                XmlNode killedTimeNode = processedData.CreateElement("KilledTime");
+                killedTimeNode.AppendChild(processedData.CreateTextNode(DateTime.Now.ToString()));
+                processNode.AppendChild(killedTimeNode);
+
+                processedData.Save("ProcessKilled.xml");
+
+
+            }
+            return true;
+        }
+    }
+}
---
+++ MonitorProcessKiller/Program.cs
@@ -0,0 +1,33 @@
+﻿using MonitorProcessKiller;
+
+public class Program
+{
+    public static int MinuteCount { get; set; }
+    public static void Main(string[] args)
+    {
+        string name = args[0];
+        int maxLifeTime = Convert.ToInt32(args[1]);
+        int freq = Convert.ToInt32(args[2]);
+        var killer = new ProcessKiller(name, maxLifeTime, freq);
+
+        //Rerun the function after every "intervalTime" minute
+        var timer = new System.Timers.Timer(freq * 60000);
+        timer.Elapsed += (s, e) =>
+        {
+            killer.KillProcess();
+            MinuteCount++;
+            if (MinuteCount == 1)
+            {
+                Console.WriteLine("One minute passed !");
+                MinuteCount = 0;
+            }
+        };
+        //timer.Elapsed += Timer_Elapsed;
+        timer.Enabled = true;
+        timer.AutoReset = true;
+        timer.Start();
+        Console.WriteLine("Press any key to stop the program");
+        Console.ReadKey();
+    }
+
+}
